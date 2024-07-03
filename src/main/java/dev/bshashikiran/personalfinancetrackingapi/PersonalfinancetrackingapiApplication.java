984e2362f8169109ff09163ba5297bdff347a630
Shashi Kiran <shashi.bolleddula@magicbricks.com>
2024-07-03 15:08:27 +0530

    Initial Project Setup

---
+++ src/main/java/dev/bshashikiran/personalfinancetrackingapi/PersonalfinancetrackingapiApplication.java
@@ -0,0 +1,13 @@
+package dev.bshashikiran.personalfinancetrackingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinancetrackingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinancetrackingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bshashikiran/personalfinancetrackingapi/PersonalfinancetrackingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bshashikiran.personalfinancetrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalfinancetrackingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
